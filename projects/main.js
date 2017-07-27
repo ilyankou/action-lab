92129b113e02e54982cb123c94756eaf79fa7cc2
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2017-07-27 19:22:41 -0400

    Modify project display

--- projects/main.js
+++ projects/main.js
@@ -1,8 +1,8 @@
 var partnersURL = 'https://docs.google.com/spreadsheets/d/1y_FVjRDYBBpUN4Pxf5iMjlAJH4bx9qhdzPoLw9fiU0A/pubhtml';
-var studentsURL = '';
-var facultyURL = '';
+var studentsURL = 'https://docs.google.com/spreadsheets/d/1xb2gFO1So3U1r6mYPJ28XHDw7s-M2h49kSFHzbJODMI/pubhtml';
+var facultyURL = 'https://docs.google.com/spreadsheets/d/1TNppz6r-A8lC1tA5zwO_3AqRGYkNX-dgwazEIpEtudc/pubhtml';
 
-WHOSE_FIRST_CHOICE = false;
+SHOW_NAMES = true;
 
 Tabletop.init({
   key: partnersURL,
@@ -31,11 +31,16 @@ function processData(data, tabletop) {
     $(div).append('<p><span>ORGANIZATION</span><br>' + org + '</p>');
     $(div).append('<p><span>PROJECT</span><br>' + project + '</p>');
     $(div).append('<p><span>IMPACT</span><br>' + impact + '</p>');
-
+    $(div).append('<p class="additional"></p>');
   }
 
+  processStudentsAndFaculty('f');
+}
+
+
+function processStudentsAndFaculty(who) {
   Tabletop.init({
-    key: studentsURL,
+    key: who == 's' ? studentsURL : facultyURL,
     simpleSheet: true,
     callback: function(data) {
 
@@ -43,13 +48,14 @@ function processData(data, tabletop) {
 
       for (i in data) {
         var row = data[i];
-        var name = row['What\'s your name?'];
+        var name = row['What is your name?'];
         var keys = Object.keys(row);
 
         var choices = ['1st', '2nd', '3rd', '4th', '5th'];
 
         for (j in keys) {
           var key = keys[j];
+
           choices.forEach(function(choice) {
             if (key.indexOf(choice) > -1) {
               var letter = row[key].split(' ')[1];
@@ -69,21 +75,31 @@ function processData(data, tabletop) {
       var projectKeys = Object.keys(projects);
       for (p in projectKeys) {
         var letter = projectKeys[p];
-        var message = 'Nobody\'s first choice yet.';
+        var message = (who == 'f')
+          ? 'No faculty members have selected this project as their favorite. '
+          : 'No students have selected this project as their first choice yet.';
 
         if (projects[letter]['1st']) {
           var n = projects[letter]['1st'].length;
           if (n > 0) {
-            if (WHOSE_FIRST_CHOICE) {
-              message = 'First choice of ' + projects[letter]['1st'].join(', ') + '.';
+            if (SHOW_NAMES || who == 'f') {
+              message = projects[letter]['1st'].join(', ');
             } else {
-              message = 'First choice of ' + n + ' student' + (n == 1 ? '.' : 's.');
+              message = n + ' student' + (n == 1 ? '' : 's');
             }
+            message += (who == 'f')
+              ? ' listed this project as their favorite. '
+              : ' listed this project as their first choice.';
           }
         }
 
-        $('<p>' + message + '</p>').insertAfter('#project-' + letter + ' h1');
+        $('#project-' + letter + ' .additional').append(message);
       }
+
+      if (who == 'f') {
+        processStudentsAndFaculty('s');
+      }
+
     }
   });
 }
